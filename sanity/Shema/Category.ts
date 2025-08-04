fac5ad0902b46475bc0c0f8550b2eeb45711765b
Donald-Source2023 <donalddyusuf@gmail.com>
2025-08-04 06:02:26 -0700

    schema

--- sanity/Shema/Category.ts
+++ sanity/Shema/Category.ts
@@ -1,1 +1,16 @@
-export default 
+import { defineField } from "sanity";
+
+export default defineField({
+  name: "category",
+  description: "Category",
+  title: "Category",
+  type: "document",
+
+  fields: [
+    defineField({
+      name: "property type",
+      title: "What type of property is this?",
+      type: "string",
+    }),
+  ],
+});
